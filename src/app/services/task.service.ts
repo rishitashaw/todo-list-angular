185969db6c5bd5191e78e775a5b62954d43f5895
Theseregrets <rishitashaw49@gmail.com>
2021-10-12 22:04:53 +0530

    service created

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../Task'
+import { TASKS } from '../mock-tasks'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  getTask(): Task[] {
+    return TASKS
+  }
+}
